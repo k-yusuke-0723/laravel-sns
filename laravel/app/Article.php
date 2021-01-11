fd999fe90c365ccf479bf75df96eeca47834c110
Yusuke Kamiyashiki <k.yusuke.0723@gmail.com>
2021-01-11 17:42:44 +0900

    ・Vueコンポーネントに渡す、いいね数を計算

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Article extends Model
 {
     // クライアントが更新可能なカラム名(ホワイトリスト)
@@ -16,5 +16,24 @@ class Article extends Model
     public function user(): BelongsTo {
         // BelongsToメソッドの引数には関係するモデルの名前を文字列で渡す
         return $this->belongsTo('App\User');
+    }
+
+    public function likes(): BelongsToMany {
+
+        // 第一引数に関係するモデル名を渡す
+        // 第二引数には中間テーブルのテーブル名を渡す
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    // いいね済みか判定
+    public function isLikedBy(?User $user): bool {
+
+        return $user ? (bool)$this->likes->where('id', $user->id)->count() : false;
+    }
+
+    // いいね数カウント
+    public function getCountLikesAttribute(): int {
+
+        return $this->likes->count();
     }
 }
